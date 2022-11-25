9bd6deb1bf33b6585ce574dc7e89d4e36c4e872d
Imran Dhahbi <idhahbi@idhahbi-ltmkef8.internal.salesforce.com>
2022-11-26 02:46:12 +1100

    remove logging

--- scripts/content.js
+++ scripts/content.js
@@ -1,25 +1,20 @@
 /*
-    track mutations:
+    notes:
+    tracking mutations via MutationObserver class:
     https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
-
-    todo:
-    - remove onLoad event listener & rely solely on MutationObserver class 
 */
 
-console.error('SCRIPT EXECUTION STARTS HERE')
 window.addEventListener("load", init, false);
 
 
 function init() {
-    console.warn('DOM CONTENT LOADED')
-
     let observer = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
-            if (!mutation.addedNodes) return
+            if (!mutation.addedNodes) 
+                return;
         
             for (let i = 0; i < mutation.addedNodes.length; i++) {
-                // do things to your newly added nodes here
-                let node = mutation.addedNodes[i]
+                let node = mutation.addedNodes[i];
                 
                 let newCommentTab;
                 for (let section of parentContainer.childNodes) {
@@ -35,28 +30,23 @@ function init() {
                                             ?.childNodes[2]
                                             ?.childNodes[0]
                                             ?.childNodes[1]
-                                            ?.childNodes[2]
+                                            ?.childNodes[2];
                 
                 if (publicInputContainer) {
-                    console.log(node, publicInputContainer)
                     let publicCheckbox = publicInputContainer.querySelectorAll('input')[0];
-                    console.error('PUBLICINPUTOCNTQINER FOUND')
-                    console.log(publicCheckbox)
 
                     let isPublicChecked = publicCheckbox.checked
                     if (!isPublicChecked) {
                         publicCheckbox.click();
                         break;
-                    } else {
-                        return;
-                    }
+                    } 
                 }
-                // console.log(node)
             }
         })
     })
-      
-    let parentContainer = document.querySelector('.MEDIUM.uiTabset--base.uiTabset--task.uiTabset.oneActionsComposer.forceActionsContainer');
+    
+    const parentClass = '.MEDIUM.uiTabset--base.uiTabset--task.uiTabset.oneActionsComposer.forceActionsContainer';
+    let parentContainer = document.querySelector(parentClass);
 
     observer.observe(parentContainer, {
         childList: true,
